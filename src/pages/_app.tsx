113b3bdcf03bae600ad26c803ae5710eb30eae5b
Rémy <remy.vandyk@ovrsea.com>
2023-07-05 14:45:20 +0200

    refactor(front): on fait mumuse

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Layout } from "./layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,8 +11,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
+    
   );
 };
 
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,46 @@
+const About = () => (
+  <div>
+    <h1 className="text-5xl text-center pb-10 font-extrabold tracking-tight text-white">
+      A propos de Koikonmanj
+    </h1>
+    <p className="tracking-tight text-white px-20 text-justify">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mattis
+      tortor a ex imperdiet mollis. Ut turpis orci, sodales et faucibus at,
+      aliquam vitae augue. Interdum et malesuada fames ac ante ipsum primis in
+      faucibus. Sed mattis neque id blandit pretium. Suspendisse et risus at
+      orci suscipit cursus. Aliquam commodo metus a augue iaculis, non
+      scelerisque elit dictum. Nullam tempus dolor dui, vel imperdiet lacus
+      euismod eget. In ultrices in magna nec scelerisque. In ac quam semper,
+      varius est sed, consectetur lacus. Nam posuere rhoncus posuere. Maecenas
+      convallis tristique nibh vel congue. Vestibulum lacinia nibh quis tortor
+      tempus mattis. In ullamcorper, turpis non mollis ullamcorper, risus tortor
+      interdum diam, scelerisque molestie lacus sem nec nunc. In venenatis
+      posuere justo ac pharetra. Maecenas suscipit bibendum nisi, vulputate
+      dignissim libero posuere sed. Donec malesuada sollicitudin odio et
+      lacinia. Praesent sem justo, gravida ut felis congue, lacinia tincidunt
+      dui. Nam tristique, orci maximus egestas pretium, lacus enim dignissim
+      purus, eget ornare magna risus id odio. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Aliquam ut lorem eget orci bibendum
+      fermentum. Etiam vulputate sodales nibh eget luctus. Curabitur id neque id
+      arcu faucibus eleifend. Nulla imperdiet suscipit augue sit amet pretium.
+      Maecenas dapibus nunc at velit aliquam facilisis. Proin vel molestie
+      felis. Vestibulum bibendum ligula ac ante cursus venenatis. Nulla viverra
+      commodo lectus, nec viverra lacus bibendum eget. Sed a massa vitae odio
+      tempor tristique efficitur non elit. Quisque viverra leo non quam pulvinar
+      semper. Ut imperdiet vehicula nisl, at ornare augue varius nec. Sed porta
+      erat metus, id fermentum velit vehicula vitae. Vestibulum ante ipsum
+      primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec
+      non leo nec urna posuere malesuada. Sed ut luctus neque. Quisque in massa
+      dui. Sed in sagittis erat. Morbi pretium, orci ut malesuada pharetra, arcu
+      nulla tempor massa, non tempor lacus augue eu nunc. Maecenas tempor dictum
+      sodales. In nisi velit, dignissim a felis vitae, tempus suscipit ligula.
+      Nulla auctor ultrices dui, eget malesuada nisi cursus non. Mauris ac
+      sollicitudin ipsum. In sit amet lectus sit amet leo suscipit ullamcorper.
+      Mauris metus neque, condimentum sit amet dictum a, imperdiet quis lectus.
+      Sed finibus id nibh id sodales. Ut vulputate cursus lacus, eget semper
+      turpis semper ac.
+    </p>
+  </div>
+);
+
+export default About;
